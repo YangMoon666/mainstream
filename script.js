71ed2490de8b1e1aa5862eebd4cc53c2abb683e9
YangMoon666 <111560359+YangMoon666@users.noreply.github.com>
2022-11-21 23:16:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,63 +2,15 @@
 
 window.onload = function () {
     document.querySelector('.preloader').classList.add('_un_active');
-    var referrer_url = document.documentURI;
-    let a = referrer_url.split('#')[referrer_url.split('#').length - 1];
-    console.log('-----------');
-    console.log(referrer_url);
-    console.log(a);
-    let id_1 = document.getElementById(a);
-    console.log(id_1);
-    // console.log(id_1);
-    id_1.querySelector('.section__active').classList.toggle('_active');
-    id_1.querySelector('.section__view').classList.toggle('_active');
-    id_1.querySelector('.h3_box').classList.toggle('_active');
-    // gsap.registerPlugin(Flip);
+    var referrer_url = document.URL;
+    if (referrer_url.includes('#')) {
+        let a = referrer_url.split('#')[referrer_url.split('#').length - 1];
+        let id_1 = document.getElementById(a);
 
-    // const dur = 2;
-    // let lastItems = [];
-    // let lastIndex = -1;
-
-    // const listItems = gsap.utils.toArray('#a1 h3');
-    // listItems.forEach((item, i) => {
-    //     item.addEventListener('click', () => {
-    //         // Get all content within the clicked item
-    //         const itemTargets = gsap.utils.toArray(item.querySelectorAll('*'));
-    //         console.log(itemTargets);
-    //         // Check to see if the item is the same one as the last time
-    //         const isSameAsLast = i === lastIndex && listItems[lastIndex];
-            
-    //         // Get all the items that are changing this click
-    //         const targets = isSameAsLast ? 
-    //         listItems.concat(itemTargets) 
-    //         : listItems.concat(itemTargets.concat(lastItems));
-            
-    //         // grab the current state of the targets (before changing)
-    //         const state = Flip.getState(targets);
-            
-    //         // Animate out the last clicked item if it's not the same as the current
-    //         if(!isSameAsLast && listItems[lastIndex]) {
-    //         listItems[lastIndex].classList.remove('active');
-    //         }
-
-    //         // Toggle the display on the clicked item
-    //         listItems[i].classList.toggle('active');
-            
-    //         Flip.from(state, {
-    //         duration: dur,
-    //         ease: "power1.inOut",
-    //         absolute: true, // make things position: absolute during the flip
-    //         nested: true,   // we've got nested flipping elements (more expensive processing)
-    //         // Fade in or out the elements within the item
-    //         onEnter: elements => gsap.fromTo(elements, {opacity: 0}, {opacity: 1, duration: dur / 2, delay: dur / 2}),
-    //         onLeave: elements => gsap.fromTo(elements, {opacity: (i, el) => state.getProperty(el, "opacity")}, {opacity: 0, duration: dur / 2}),
-    //         });
-            
-    //         // Update our variables
-    //         lastItems = itemTargets;
-    //         lastIndex = i;
-    //     });
-    // });
+        id_1.querySelector('.section__active').classList.toggle('_active');
+        id_1.querySelector('.section__view').classList.toggle('_active');
+        id_1.querySelector('.h3_box').classList.toggle('_active');
+    }
 
     document.addEventListener('click', documentActions);
     function documentActions(e) {
